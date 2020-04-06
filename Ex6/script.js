066392855b4c8c3b634ab8ac510162d89b94f072
“mvinicius112015@gmail.com” <mvinicius112015@gmail.com>
2020-04-05 23:24:08 -0300

    Alterações Finais

--- Ex6/script.js
+++ Ex6/script.js
@@ -2,5 +2,5 @@ let i = 0;
 setTimeout(() => alert(i), 100); // ?
 // Assuma que essa função demore mais que 100ms para executar
 for (let j = 0; j < 100000000; j++) {
-    i++;
+    i++; 
 }
--- Ex8/script.js
+++ Ex8/script.js
@@ -1,11 +1,11 @@
-function recipe() {
-    fetch('receitas.json').then(Response => {
+async function recipe() {
+    let response = await fetch('receitas.json').then(Response => {
         return Response.json();
     }).then(data => {
         let receitas = document.getElementById('receita');
         for (var info of data.results) {
             let x = document.createElement('li');
-            x.appendChild(document.createTextNode(info.title + " "));
+            x.appendChild(document.createTextNode(info.title + " <br>"));
             x.appendChild(document.createTextNode("Ingredientes " + info.ingredients + "  "));
             x.appendChild(document.createTextNode(info.href));
             let imagem = document.createElement("img");
